73d5189118ccc8c0c0d151044ccdc1d829bfb99d
Adam R <adamroyal12@gmail.com>
2021-05-16 20:31:29 +0100

    Name can be input and registered

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'sinatra'
 gem 'launchy'
+gem 'sinatra'
 gem 'sinatra-contrib'
 
 group :test do
--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
+require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader' if development?
 
 class RockPaperScissors < Sinatra::Base
   get '/test' do
     'test page'
+  end
+
+  get '/' do
+    erb(:home)
+  end
+
+  get '/playerchoice' do
+    @name = params[:name]
+    erb(:playerchoice)
   end
 
   run! if app_file == $0
