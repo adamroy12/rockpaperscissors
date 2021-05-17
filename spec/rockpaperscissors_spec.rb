96b9622a990b26aaf8a182e33fe206fc5dd4cfe8
Adam R <adamroyal12@gmail.com>
2021-05-17 09:40:13 +0100

    can't get spec to pass for RPS but it works

--- spec/rockpaperscissors_spec.rb
+++ spec/rockpaperscissors_spec.rb
@@ -2,8 +2,12 @@ require 'rockpaperscissors.rb'
 
 describe Game do
   context 'compares player input against computer input and decides a winner' do
+
     it 'decides when the player wins' do
-      
+      game = Game.new('Rock')
+  
+      allow(game).to receive(:computer_move).and_return('Scissors')
+      expect(game.run).to eq 'player'
     end
 
     it 'decides when the computer wins' do
